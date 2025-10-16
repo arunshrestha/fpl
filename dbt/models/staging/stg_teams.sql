c7f874ae4f0d5ad23b7a86065a5d18f6339fdfe2
arunshrestha <arunshrestha2009@hotmail.com>
2025-10-15 21:33:49 -0400

    added stg_teams model in dbt

--- dbt/models/staging/stg_teams.sql
+++ dbt/models/staging/stg_teams.sql
@@ -5,16 +5,23 @@ with src as (
 )
 
 select
-    (t->>'id')::int                                    as id,
-    t->>'name'                                         as team_name,
-    t->>'short_name'                                   as short_name,
-    t->>'code'                                         as code,
-    (t->>'team_division')::int                         as team_division,
-    (t->>'strength')::int                              as strength,
-    (t->>'strength_attack_home')::int                  as strength_attack_home,
-    (t->>'strength_attack_away')::int                  as strength_attack_away,
-    (t->>'strength_defence_home')::int                 as strength_defence_home,
-    (t->>'strength_defence_away')::int                 as strength_defence_away,
-    (t->>'pulse_id')::int                              as pulse_id
+    (t->>'id')::int                                      as team_id,
+    nullif(t->>'code','')::int                           as code,
+    t->>'name'                                           as team_name,
+    t->>'short_name'                                     as team_short_name,
+    nullif(t->>'position','')::int                       as team_position,
+    nullif(t->>'played','')::int                         as played,
+    nullif(t->>'win','')::int                            as wins,
+    nullif(t->>'draw','')::int                           as draws,
+    nullif(t->>'loss','')::int                           as losses,
+    nullif(t->>'points','')::int                         as points,
+    nullif(t->>'strength','')::int                       as strength,
+    nullif(t->>'pulse_id','')::int                       as pulse_id,
+    nullif(t->>'strength_overall_home','')::int          as strength_overall_home,
+    nullif(t->>'strength_overall_away','')::int          as strength_overall_away,
+    nullif(t->>'strength_attack_home','')::int           as strength_attack_home,
+    nullif(t->>'strength_attack_away','')::int           as strength_attack_away,
+    nullif(t->>'strength_defence_home','')::int          as strength_defence_home,
+    nullif(t->>'strength_defence_away','')::int          as strength_defence_away
 from src
 join lateral jsonb_array_elements((data->'teams')) as t on true
