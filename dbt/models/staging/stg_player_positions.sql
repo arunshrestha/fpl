2511cf3e4b81eaf81715e528add85772b08ac29b
arunshrestha <arunshrestha2009@hotmail.com>
2025-10-15 21:24:58 -0400

    added player_positions model in dbt

--- dbt/models/staging/stg_player_positions.sql
+++ dbt/models/staging/stg_player_positions.sql
@@ -5,11 +5,16 @@ with src as (
 )
 
 select
-    (et->>'id')::int                       as id,
-    et->>'singular_name'                   as player_position,
-    et->>'singular_name_short'             as player_position_short,
-    et->>'plural_name'                     as player_position_plural,
-    et->>'plural_name_short'               as player_position_plural_short,
-    (et->>'element_count')::int            as element_count
+    (et->>'id')::int                        as player_position_id,
+    et->>'singular_name'                    as player_position_singular_name,
+    et->>'singular_name_short'              as player_position_singular_name_short,
+    et->>'plural_name'                      as player_position_plural_name,
+    et->>'plural_name_short'                as player_position_plural_name_short,
+    nullif(et->>'squad_select','')::int     as squad_select,
+    nullif(et->>'squad_min_select','')::int as squad_min_select,
+    nullif(et->>'squad_max_select','')::int as squad_max_select,
+    nullif(et->>'squad_min_play','')::int   as squad_min_play,
+    nullif(et->>'squad_max_play','')::int   as squad_max_play,
+    (et->>'element_count')::int             as element_count
 from src
 join lateral jsonb_array_elements((data->'element_types')) as et on true
